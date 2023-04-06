279a3158057f25d9a350a60cb0d325f33ff5024e
Corey <coreybasden@gmail.com>
2023-04-06 09:34:24 -0700

    fleshed out Television and added 2nd packadge.

--- src/cscd212classes/lab2/Television.java
+++ src/cscd212classes/lab2/Television.java
@@ -1,4 +1,11 @@
 package cscd212classes.lab2;
 
-public class Television {
+public class Television
+{
+    private final boolean fourK; // private final data member if the Television is a 4K television
+    private final String make; // private final data member for the Television make
+    private final String model; // private final data member for the Television model
+    private final int resolution; // private final data member for the Television resolution
+    private final int screenSize; // private final data member for the Television screenSize
+    private final boolean smart; // private final data member if the Television is a smart television
 }
